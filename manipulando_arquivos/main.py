46cdfdd6d13dc11c408b4b4e7a156c128d45ab9f
Aluno <aluno@estacio.acad>
2026-05-25 17:44:33 -0300

    meu projeto

---
+++ manipulando_arquivos/main.py
@@ -0,0 +1,28 @@
+from manipulando_arquivos.menu import menu
+
+def main():
+
+    nomes = []
+
+    while True:
+        opcao = menu()
+        if opcao == "1":
+            nome = input("Digite o seu nome:").strip().title()
+            nomes.append(nome)
+
+            with open("cadastros.txt", "a") as arquivo:
+
+                arquivo.write(nome + "\n")
+        elif opcao == "2":
+            with open("cadastros.txt", "r") as arquivo:
+                print(arquivo.read())
+     
+            print(f"cadastrados: {nomes}")      
+        elif opcao == "3":
+            print("Saindo do programa...")
+            break 
+        else:
+            print("Opção inválida. Tente novamente.")
+
+if __name__ == "__main__":
+    main()
